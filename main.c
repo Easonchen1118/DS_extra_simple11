f42513c288a9375ffe8a2f1e57e511763ead30ec
Easonchen1118 <120409122+Easonchen1118@users.noreply.github.com>
2022-12-20 21:25:19 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//矩陣反轉
+int main(){
+    int row, col;
+    scanf("%d %d", &row, &col);
+    int inpt[row][col];
+    int oupt[col][row];
+    for (int i=0; i<row; i++) {
+        for (int j=0; j<col; j++) {
+            scanf("%d", &inpt[i][j]);
+            oupt[j][i] = inpt[i][j];
+        }
+    }
+    for (int i=0; i<col; i++) {
+        for (int j=0; j<row; j++) {
+            printf("%d", oupt[i][j]);
+            if (j != row -1) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
